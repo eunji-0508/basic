e48010a5b1aa9b2a899efbd28a6583266516774b
eunji-0508 <cojia@naver.com>
2025-07-28 10:23:21 +0900

    first commit

---
+++ src/main/java/org/example/calculator/App.java
@@ -0,0 +1,52 @@
+package org.example.calculator;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Calculator2 calculator = new Calculator2();
+
+        while(true) {
+            System.out.print("첫번째 숫자를 입력하세요: ");
+            int firstNum = scanner.nextInt();
+            if(firstNum < 0) {
+                System.out.println("음수는 입력할 수 없습니다. 프로그램을 재시작합니다.");
+                continue;
+            }
+
+            System.out.print("두번째 숫자를 입력하세요: ");
+            int secondNum = scanner.nextInt();
+            if(secondNum < 0) {
+                System.out.println("음수는 입력할 수 없습니다. 프로그램을 재시작합니다.");
+                continue;
+            }
+
+            System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+
+            double result = calculator.calculate(firstNum, secondNum, operator);
+
+            System.out.println("결과 : " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            if("exit".equals(scanner.next())) {
+                break;
+            }
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하겠습니까? (yes 입력 시 삭제)");
+            if("yes".equals(scanner.next())) {
+                calculator.removeResult();
+            }
+
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (yes 입력 시 조회)");
+            if("yes".equals(scanner.next())) {
+                List<Double> resultList = calculator.getResultList();
+                System.out.println("저장된 연산결과 : " + resultList.toString());
+            }
+        }
+        // scanner.close();
+    }
+}
---
+++ src/main/java/org/example/calculator/Calculator2.java
@@ -0,0 +1,49 @@
+package org.example.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator2 {
+    private final List<Double> resultList = new ArrayList<>();
+
+    public double calculate(int firstNum, int secondNum, char operator) {
+        double result;
+        switch(operator) {
+            case '+':
+                result = firstNum + secondNum;
+                break;
+
+            case '-':
+                result = firstNum - secondNum;
+                break;
+
+            case '*':
+                result = firstNum * secondNum;
+                break;
+
+            case '/':
+                if(secondNum == 0) {
+                    throw new IllegalArgumentException("나눗셈 연산에서 분모(두 번째 정수)가 0일 수 없습니다!");
+                }
+                result = (double) firstNum / secondNum;
+                break;
+
+            default:
+                throw new IllegalArgumentException("사칙연산 기호는 +, -, *, / 중 하나여야 합니다.");
+        }
+        addResult(result);
+        return result;
+    }
+
+    public List<Double> getResultList() {
+        return resultList;
+    }
+
+    public void addResult(double result) {
+        resultList.add(result);
+    }
+
+    public void removeResult(){
+        resultList.remove(0);
+    }
+}
